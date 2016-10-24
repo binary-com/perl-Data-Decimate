0fd35b6f648962752b89158cbaf149eacee20098
Shahrizal Shaari <shahrizal@binary.com>
2016-10-24 03:06:19 +0000

    update .t files

--- lib/Data/Resample/ResampleCache.pm
+++ lib/Data/Resample/ResampleCache.pm
@@ -1,7 +1,28 @@
 package Data::Resample::ResampleCache;
 
+use strict;
+use warnings;
+
+use 5.010;
 use Moose;
 
+=head1 SUBROUTINES/METHODS
+
+=head2 resample_cache_backfill
+
+=cut
+
+sub resample_cache_backfill {
+
+}
+
+=head2 resample_cache_get
+
+=cut
+
+sub resample_cache_get {
+
+}
 
 
 no Moose;
--- lib/Data/Resample/TicksCache.pm
+++ lib/Data/Resample/TicksCache.pm
@@ -1,6 +1,28 @@
 package Data::Resample::TicksCache;
 
+use strict;
+use warnings;
+
+use 5.010;
 use Moose;
+
+=head1 SUBROUTINES/METHODS
+
+=head2 tick_cache_insert
+
+=cut
+
+sub tick_cache_insert {
+
+}
+
+=head2 tick_cache_get_num_ticks
+
+=cut
+
+sub tick_cache_get_num_ticks {
+
+}
 
 no Moose;
 
