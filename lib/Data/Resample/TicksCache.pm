5ff8a466bd13dff8231474ba96829f550925dd19
Shahrizal Shaari <shahrizal@binary.com>
2016-10-24 05:19:10 +0000

    insert function for TicksCache

--- lib/Data/Resample/TicksCache.pm
+++ lib/Data/Resample/TicksCache.pm
@@ -15,7 +15,16 @@ extends 'Data::Resample';
 =cut
 
 sub tick_cache_insert {
+    my ($self, $tick, $fast_insert) = @_;
 
+    $tick = $tick->as_hash if blessed($tick);
+
+    my %to_store = %$tick;
+
+    $to_store{count} = 1;    # These are all single ticks;
+    my $key = $self->_make_key($to_store{symbol}, 0);
+
+    return _update($self->_redis, $key, $tick->{epoch}, $encoder->encode(\%to_store), $fast_insert);
 }
 
 =head2 tick_cache_get_num_ticks
