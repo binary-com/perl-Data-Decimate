91ca1e984b8678197857aeb93da7128be3daff98
Shahrizal Shaari <shahrizal@binary.com>
2016-10-24 06:26:01 +0000

    add func for resample get

--- lib/Data/Resample/ResampleCache.pm
+++ lib/Data/Resample/ResampleCache.pm
@@ -6,6 +6,8 @@ use warnings;
 use 5.010;
 use Moose;
 
+use List::Util qw( first min max );
+
 extends 'Data::Resample';
 
 =head1 SUBROUTINES/METHODS
@@ -23,7 +25,28 @@ sub resample_cache_backfill {
 =cut
 
 sub resample_cache_get {
+    my ($self, $args) = @_;
 
+    my $which = $args->{symbol};
+    my $start = $args->{start_epoch};
+    my $end   = $args->{end_epoch} || time;
+
+    my $ti = 31;
+
+    my $redis = $self->_redis;
+
+    my @res;
+
+    my ($hold_secs, $key);
+
+    $hold_secs = 0;                       #agg retention interval, sec;
+    $key = $self->_make_key($which, 1);
+
+    my $start = $end - min($ti->seconds, $hold_secs);
+
+    @res = map { $self->decoder->decode($_) } @{$redis->zrangebyscore($key, $start, $end)};
+
+    return \@res;
 }
 
 no Moose;
